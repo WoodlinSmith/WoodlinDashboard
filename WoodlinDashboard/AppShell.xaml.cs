ef856e363edfb4a64407ee6ba9be67fbc1b20003
woodl <woodl@LAPTOP-8GJIQ44B>
2023-10-27 22:56:06 -0500

    Add project files.

---
+++ WoodlinDashboard/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WoodlinDashboard;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WoodlinDashboard/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WoodlinDashboard;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

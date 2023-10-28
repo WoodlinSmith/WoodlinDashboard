53962f24e8fa33e0c114b8317d709861d354c2a6
Woodlin Smith <woodlinsmith@gmail.com>
2023-10-27 23:20:39 -0500

    initial data classes

---
+++ DashboardLib/Models/Data/GameRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardLib.Models.Data
+{
+    public class GameRecord
+    {
+        public GameRecord()
+        {
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public decimal RegularPrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public double TotalDiscount { get; set; }
+
+    }
+}
